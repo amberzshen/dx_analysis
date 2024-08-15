d12d5817b1ed24571d3e87033172b30a05f8e7c5
amberzshen <amberzshen@gmail.com>
2024-08-15 11:02:02 -0400

    bug fix get_mat

--- get_genotype_mat/run_get_mat.sh
+++ get_genotype_mat/run_get_mat.sh
@@ -5,15 +5,12 @@ set -euo pipefail
 region=$1
 out_prefix=$2
 
-# vcf_file="/mnt/project/Bulk/Previous WGS releases/GATK and GraphTyper WGS/SHAPEIT Phased VCFs/ukb20279_c${chrom}_b0_v1.vcf.gz"
-
 mkdir -p tmp
 mkdir -p genotype_matrices/matrices
 mkdir -p genotype_matrices/variant_metadata
 
 pip install --upgrade scipy
 pip install cyvcf2
-# bcftools view -Ob -r $region "$vcf_file" > "tmp/${out_prefix}_${region}.bcf" # stream region of chromosome to worker
 python3 get_mat.py $region $out_prefix
 
 rm -r tmp
--- get_genotype_mat/submit_job_get_mat.sh
+++ get_genotype_mat/submit_job_get_mat.sh
@@ -7,7 +7,7 @@ dx run app-swiss-army-knife \
     -iin="amber/scripts/get_mat.py" \
     -icmd="bash run_get_mat.sh $region $out_prefix" \
     --destination "/" \
-    --instance-type "mem2_ssd1_v2_x4" \
+    --instance-type "mem1_ssd1_v2_x4" \
     --priority low \
     --name get_mat_test \
     -y
