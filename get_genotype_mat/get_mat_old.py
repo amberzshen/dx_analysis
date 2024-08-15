32638de1affc1c648a04e76ab40bc9820a9fdc6e
amberzshen <amberzshen@gmail.com>
2024-08-15 10:38:16 -0400

    update get_mat

--- get_genotype_mat/get_mat_old.py
+++ get_genotype_mat/get_mat_old.py
@@ -8,25 +8,18 @@ def vcf_to_csc(region: str, out_prefix: str, phased: bool = False, flip_minor_al
     Codes unphased genotypes as 0/1/2/3, where 3 means that at least one of the two alleles is unknown.
     Codes phased genotypes as 0/1, and there are 2n rows, where rows 2*k and 2*k+1 correspond to individual k.
     """
-    chrom = region.split(':')[0]
-    vcf_file=f'/mnt/project/Bulk/Previous WGS releases/GATK and GraphTyper WGS/SHAPEIT Phased VCFs/ukb20279_c{chrom}_b0_v1.vcf.gz'
-    vcf = VCF(vcf_file, gts012=True, strict_gt=True)
+    vcf = VCF(f'tmp/{out_prefix}_{region}.bcf', gts012=True, strict_gt=True)
+    chrom = region.split('chr')[1].split(':')[0]
     data = []
     idxs = []
     ptrs = [0]
+    var_metadata = []
     flip = False
     
     ploidy = 1 if phased else 2
-    
-    variant_metadata_path = f'genotype_matrices/variant_metadata/{out_prefix}_{region}.txt'
-    with open(variant_metadata_path, 'w') as f:
-        f.write("##fileformat=PVARv1.0\n")
-        f.write("##INFO=<ID=IDX,Number=1,Type=Integer,Description=\"Variant Index\">\n")
-        f.write("##INFO=<ID=FLIP,Number=1,Type=Integer,Description=\"Flip Information\">\n")
-        f.write("#CHROM POS ID REF ALT INFO\n")
 
     # TODO: handle missing data
-    for var in vcf(region):
+    for var in vcf:
         if phased:
             gts = np.ravel(np.asarray(var.genotype.array())[:, :2])
         else:
@@ -44,10 +37,7 @@ def vcf_to_csc(region: str, out_prefix: str, phased: bool = False, flip_minor_al
         idxs.append(idx)
         ptrs.append(ptrs[-1] + len(idx))
         var_metadata.append(' '.join([chrom, str(var.POS), '.', var.REF, ','.join(var.ALT), f'IDX={len(var_metadata)};FLIP={int(flip)}']))
-        
-        f = open(variant_metadata_path, 'a')
-        f.write(' '.join([chrom, str(var.POS), '.', var.REF, ','.join(var.ALT), f'IDX={len(var_metadata)};FLIP={int(flip)}'])+'\n')
-        f.close()
+        # var_metadata.append(' '.join([str(var.POS), var.REF, ','.join(var.ALT), str(var.QUAL), str(flip)]))
         
     data = np.concatenate(data)
     idxs = np.concatenate(idxs)
@@ -55,6 +45,14 @@ def vcf_to_csc(region: str, out_prefix: str, phased: bool = False, flip_minor_al
     genotypes = scipy.sparse.csc_matrix((data, idxs, ptrs))
     
     scipy.sparse.save_npz(f'genotype_matrices/matrices/{out_prefix}_{region}.npz', genotypes)
+    
+    with open(f'genotype_matrices/variant_metadata/{out_prefix}_{region}.txt', 'w') as file:
+        file.write("##fileformat=PVARv1.0\n")
+        file.write("##INFO=<ID=IDX,Number=1,Type=Integer,Description=\"Variant Index\">\n")
+        file.write("##INFO=<ID=FLIP,Number=1,Type=Integer,Description=\"Flip Information\">\n")
+        file.write("#CHROM POS ID REF ALT INFO\n")
+        for var in var_metadata:
+            file.write(var + "\n")
 
 
 
@@ -65,6 +63,6 @@ if __name__ == "__main__":
     parser.add_argument('out_prefix', type=str)
     args = parser.parse_args()
     
-    vcf_to_csc(args.chrom, args.region, args.out_prefix, phased=True, flip_minor_alleles=False)
+    vcf_to_csc(args.region, args.out_prefix, phased=True, flip_minor_alleles=False)
     
 
--- get_genotype_mat/run_get_mat.sh
+++ get_genotype_mat/run_get_mat.sh
@@ -6,7 +6,7 @@ chrom=$1
 region=$2
 out_prefix=$3
 
-vcf_file="/mnt/project/Bulk/Previous WGS releases/GATK and GraphTyper WGS/SHAPEIT Phased VCFs/ukb20279_c${chrom}_b0_v1.vcf.gz"
+# vcf_file="/mnt/project/Bulk/Previous WGS releases/GATK and GraphTyper WGS/SHAPEIT Phased VCFs/ukb20279_c${chrom}_b0_v1.vcf.gz"
 
 mkdir -p tmp
 mkdir -p genotype_matrices/matrices
@@ -14,7 +14,7 @@ mkdir -p genotype_matrices/variant_metadata
 
 pip install --upgrade scipy
 pip install cyvcf2
-bcftools view -Ob -r $region "$vcf_file" > "tmp/${out_prefix}_${region}.bcf" # stream region of chromosome to worker
+# bcftools view -Ob -r $region "$vcf_file" > "tmp/${out_prefix}_${region}.bcf" # stream region of chromosome to worker
 python3 get_mat.py $region $out_prefix
 
 rm -r tmp
