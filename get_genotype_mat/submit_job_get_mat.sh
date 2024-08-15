13be2dd07d1e5d3ec564358d27ff396de90a30ea
amberzshen <amberzshen@gmail.com>
2024-08-15 11:18:51 -0400

    bug fix get_mat

--- get_genotype_mat/submit_job_get_mat.sh
+++ get_genotype_mat/submit_job_get_mat.sh
@@ -3,8 +3,8 @@ region=$1
 out_prefix=$2
 
 dx run app-swiss-army-knife \
-    -iin="/amber/scripts/run_get_mat.sh" \
-    -iin="amber/scripts/get_mat.py" \
+    -iin="/amber/scripts/get_genotype_mat/run_get_mat.sh" \
+    -iin="amber/scripts/get_genotype_mat/get_mat.py" \
     -icmd="bash run_get_mat.sh $region $out_prefix" \
     --destination "/" \
     --instance-type "mem1_ssd1_v2_x4" \
