d5518667e05cbe1c0a2771f8cfa47a59acda2d63
amberzshen <amberzshen@gmail.com>
2024-08-15 13:59:16 -0400

    linarg_pipeline bug fix

--- linarg_pipeline/submit_job_linarg_pipeline.sh
+++ linarg_pipeline/submit_job_linarg_pipeline.sh
@@ -4,10 +4,11 @@ data_identifier=$1
 dx run app-swiss-army-knife \
     -iin="/amber/scripts/run_linarg_pipeline.sh" \
     -iin="amber/scripts/linarg_pipeline.py" \
-    -inn="genotype_matrices/${data_identifier}.npz" \
+    -inn="/genotype_matrices/matrices/${data_identifier}.npz" \
+    -inn="/genotype_matrices/variant_metadata/${data_identifier}.txt" \
     -icmd="bash run_linarg_pipeline.sh $data_indentifier" \
     --destination "/" \
     --instance-type "mem1_ssd1_v2_x4" \
     --priority low \
-    --name run_linarg_pipelie_test \
+    --name run_linarg_pipeline_test \
     -y
