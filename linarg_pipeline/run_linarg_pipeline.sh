35917b7eb4667f6e655af65819d5d8e3b55c585e
amberzshen <amberzshen@gmail.com>
2024-08-16 17:02:11 -0400

    change branch to amber_debug

--- linarg_pipeline/run_linarg_pipeline.sh
+++ linarg_pipeline/run_linarg_pipeline.sh
@@ -11,7 +11,7 @@ python3.9 -m pip install --upgrade scipy
 python3.9 -m pip install cyvcf2
 python3.9 -m pip install dxpy # for dna_nexus.py
 python3.9 -m pip install pyspark # for dna_nexus.py
-python3.9 -m pip install git+https://github.com/quattro/linear-dag.git
+python3.9 -m pip install git+https://github.com/quattro/linear-dag.git@amber_debug
 
 mkdir -p linear_args/adjacency_matrices
 mkdir -p linear_args/statistics
