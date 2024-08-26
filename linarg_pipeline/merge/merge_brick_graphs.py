b5d7d57e69c2c38ee557f093d09cbb86a3e25a58
amberzshen <amberzshen@gmail.com>
2024-08-26 17:39:45 -0400

    merge brick graph pipeline

--- linarg_pipeline/merge/merge_brick_graphs.py
+++ linarg_pipeline/merge/merge_brick_graphs.py
@@ -1,5 +1,6 @@
 import linear_dag as ld
 from linear_dag.brick_graph import merge_brick_graphs
+from linear_dag.one_summed_cy import linearize_brick_graph
 import argparse
 import os
 
@@ -11,7 +12,7 @@ merged_graph, variant_indices, num_samples, index_mapping = merge_brick_graphs(f
 linear_arg_adjacency_matrix = linearize_brick_graph(merged_graph)
 variants_flipped_ref_alt = np.zeros(len(variant_indices), dtype=bool) # may have to change this eventually
 sample_idx = np.arange(num_samples)
-linarg = ld.LinearARG(linear_arg_adjacency_matrix, sample_idx, merged_variant_indices, variants_flipped_ref_alt)
+linarg = ld.LinearARG(linear_arg_adjacency_matrix, sample_idx, variant_indices, variants_flipped_ref_alt)
 
 files = os.listdir(f'/mnt/project/linear_arg_results/{args.data_identifier}/variant_metadata/')
 ind_arr = np.array([int(f.split('_')[0]) for f in files])
@@ -31,5 +32,6 @@ for var_meta in files:
             alts.append(items[4])
 
 chrom = args.data_identifier.split('_')[1].split('chr')[1].split(':')[0]
-linarg.write(f'/mnt/project/linear_arg_results/{args.data_identifier}/linear_arg', chrom, positions, refs, alts)
+linarg.write(f'{args.data_identifier}/linear_arg', chrom, positions, refs, alts.
+             sample_filename=f'{args.data_identifier}/linear_arg')
 
---
+++ linarg_pipeline/merge/submit_job_merge_brick_graphs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+data_identifier=$1
+instance_type=$2
+
+dx run app-swiss-army-knife \
+    -iin="/amber/scripts/run_merge_brick_graphs.sh" \
+    -iin="amber/scripts/merge_brick_graphs.py" \
+    -icmd="bash run_merge_brick_graphs.sh $partition_identifier" \
+    --destination "/linear_arg_results/" \
+    --instance-type $instance_type \
+    --priority low \
+    --name "linarg_merge_${data_identifier}_${partition_identifier}" \
+    --brief \
+    -y
